9a9a7c8554e74c4966ae161d10f27c9875fc2cd4
nagi91204 <nonaka912.kdgo.student@gmail.com>
2026-06-28 22:18:08 +0900

    Prepare for Render deployment

--- kakeibo_project/settings.py
+++ kakeibo_project/settings.py
@@ -35,9 +35,12 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    ...
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'kakeibo_project.urls'
 
 TEMPLATES = [
