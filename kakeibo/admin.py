d6c579ef9cfec928ce0ca4164e510867e272f082
nagi91204 <nonaka912.kdgo.student@gmail.com>
2026-06-28 22:04:49 +0900

    commit

--- kakeibo/admin.py
+++ kakeibo/admin.py
@@ -6,6 +6,3 @@ from .models import Transaction
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ['date', 'transaction_type', 'category', 'amount', 'memo']
     list_filter = ['transaction_type', 'category']
-
-    # admin@admin.com
-    # admin@admin
