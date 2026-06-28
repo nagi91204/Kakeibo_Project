a9f37fb2c9d1d10321a64a13f05d68225f3d94fe
nagi91204 <nonaka912.kdgo.student@gmail.com>
2026-06-28 22:55:00 +0900

    clean requirements for Render deploy

--- kakeibo_project/settings.py
+++ kakeibo_project/settings.py
@@ -66,10 +66,9 @@ WSGI_APPLICATION = 'kakeibo_project.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
     )
 }
-
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
