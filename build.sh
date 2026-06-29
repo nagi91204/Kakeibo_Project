7e94f44d635dac6985cab8abfac06717913e9044
nagi91204 <nonaka912.kdgo.student@gmail.com>
2026-06-29 11:56:19 +0900

    fix prod settings

--- build.sh
+++ build.sh
@@ -6,3 +6,18 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+	python manage.py shell <<'PY'
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
+
+if username and password and not User.objects.filter(username=username).exists():
+	User.objects.create_superuser(username=username, email=email, password=password)
+PY
+fi
